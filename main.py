35300167f349ed3e91b3c14f926f488bcdb17ddd
ricardo llanos <ricardollanos711@gmail.com>
2026-03-11 15:08:42 -0500

    funcionalities were created and incorporated to main

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
+from register import register
+from operations import calculation
+from summary import summary_generation
 print("*** SALES RECORDING SYSTEM ***")
 print("\n")
 
-def system():    
-    income = True
+record = [] 
+
+# A variable is generated to get the number of products to be recorded
+register(record)
+
+
+status = "si"
+while status == "si":
+    status = str(input("\nDo you want to register another profuct? si / no : "))
     
+    print("\n")
+    if status == "no":
+       total_amount = calculation(record)
        
-    while income:
-        record = []
-        contador = 1
-        for i in range(contador):
+       summary_generation(record, total_amount)
 
-        # A variable is generated to get the number of products to be recorded
+    else:
+        register(record)
+
+
+
+
+
+
+                
             
-            print(f"Record product informacion #{i + 1}") 
-            name_product = str(input("Product name: "))
-            price = float(input("Price: "))
-            quantity = int(input("Quantity: "))
-            print("\n")
-                # A dictionary is created to store product information
-            product = {
-                    "name": name_product,
-                    "price": price,
-                    "quantity": quantity
-
-                }
-                
-                # The new product was added to the list called record
-            record.append(product)
-                
-            income = str(input("Do you want to register another product? yes/no: "))
-            if income == "yes":
-                    contador=+ 1
-            else: 
-                break
-    print(record)
-
-
-system()
+    
 
 
                 
