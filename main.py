2b357e450131ff74e4f6223c8fcb0dbf016ccb8e
ricardo llanos <ricardollanos711@gmail.com>
2026-03-12 14:16:06 -0500

    correction

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ print("*** SALES RECORDING SYSTEM ***")
 print("\n")
 
 record = [] 
-
 # A variable is generated to get the number of products to be recorded
 register(record)
 
-
-status = "si"
-while status == "si":
-    status = str(input("\nDo you want to register another profuct? si / no : "))
-    
+status = "yes"
+while status == "yes":
+    status = str(input("\nDo you want to register another profuct? yes / no : ").lower())
     print("\n")
+    while status not in ["no", "yes"]:
+        print("ERROR! Ingresar solamente la opcion (yes) o (no)")
+        status = (input("\nDo you want to register another profuct? yes / no : ").lower())
+        print()
     if status == "no":
        total_amount = calculation(record)
        
        summary_generation(record, total_amount)
-
+    
     else:
         register(record)
 
